739132332ee133b04e50cbcbaf1c075c1069f0c3
Arjun kumar vs <arjun.tech177@gmail.com>
2024-03-23 16:53:57 +0530

    Ui created

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,19 @@
+
+import Banner from '../Components/Banner/Banner';
+import Footer from '../Components/Footer/Footer';
+import Header from '../Components/Header/Header';
+import Posts from '../Components/Posts/Posts';
+
+function Home() {
+  return (
+    <div className="homeParentDiv">
+      <Header/>
+      <Banner />
+      <Posts />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
+ 
